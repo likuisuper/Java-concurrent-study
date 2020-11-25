4fea47f77e0358ccf706f006740da1907c5d5184
cxylk <cxylikui@163.com>
2020-11-25 22:58:06 +0800

    :tada: ThreadLocal中的继承与不可继承

--- basics/src/com/cxylk/thread/threadcommunication/InheritableThreadLocal.java
+++ basics/src/com/cxylk/thread/threadcommunication/InheritableThreadLocal.java
@@ -1,14 +1,16 @@
 package com.cxylk.thread.threadcommunication;
 
 /**
- * @Classname NoInheritableThreadLocal
- * @Description ThreadLocal不支持继承性
+ * @Classname InheritableThreadLocal
+ * @Description 支持继承的ThreadLocal,它继承自ThreadLocal,提供了一个特性，
+ *              就是让子线程可以访问到父线程中设置的本地变量
  * @Author likui
- * @Date 2020/11/25 22:24
+ * @Date 2020/11/25 22:35
  **/
-public class NoInheritableThreadLocal {
+public class InheritableThreadLocal {
     //1.创建线程变量
-    private static ThreadLocal<String> threadLocal=new ThreadLocal<>();
+    //只需将ThreadLocal改成InheritableThreadLocal即可
+    private static ThreadLocal<String> threadLocal=new java.lang.InheritableThreadLocal<>();
 
     public static void main(String[] args) {
         //2.设置线程变量(注意，set()方法中当前线程在这里为main线程)
@@ -18,8 +20,8 @@ public class NoInheritableThreadLocal {
         //子线程输出线程变量的值
         Thread thread=new Thread(()->{
             //获取的是当前线程(即thread)中threadLocals对应变量的值
-            //也就是说子线程获取不到父线程中设置的值
-            System.out.println("thread:"+threadLocal.get());//输出null
+            //现在子线程可以访问到父线程中设置的本地变量值
+            System.out.println("thread:"+threadLocal.get());//输出hello
         });
 
         thread.start();
