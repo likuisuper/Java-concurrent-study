271d9f9c9243c52b1735624b69df3f5bd57cf12b
cxylk <cxylikui@163.com>
2020-11-25 22:26:08 +0800

    :construction: ThreadLocal的使用

---
+++ basics/src/com/cxylk/thread/threadcommunication/NoInheritableThreadLocal.java
@@ -0,0 +1,10 @@
+package com.cxylk.thread.threadcommunication;
+
+/**
+ * @Classname NoInheritableThreadLocal
+ * @Description TODO
+ * @Author likui
+ * @Date 2020/11/25 22:24
+ **/
+public class NoInheritableThreadLocal {
+}
